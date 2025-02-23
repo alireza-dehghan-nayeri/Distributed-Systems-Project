30c0ef8c7ce18e86d7f0f3d0c70226180d361741
Alireza Dehghan Nayeri <alireza79.nayeri@gmail.com>
2025-02-23 19:27:50 +0200

    add /metrics to services without flask

--- services/function-executor/main.py
+++ services/function-executor/main.py
@@ -1,4 +1,6 @@
+from prometheus_client import start_http_server
 from app.app import kafka_listener
 
 if __name__ == "__main__":
+    start_http_server(8000)  # Exposes /metrics at http://localhost:8000
     kafka_listener()
