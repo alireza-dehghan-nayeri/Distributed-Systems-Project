21343c9891a8effc869d96e69a8a20b6166fea79
Alireza Dehghan Nayeri <alireza79.nayeri@gmail.com>
2025-02-23 13:01:23 +0200

    added change feed to init_db.sql

--- services/function-database/init_db.sql
+++ services/function-database/init_db.sql
@@ -10,18 +10,16 @@ CREATE TABLE IF NOT EXISTS functions (
     name STRING UNIQUE NOT NULL,
     state STRING NOT NULL,
     code STRING NOT NULL,
-    requirements STRING NOT NULL,
-    deployment_yaml STRING DEFAULT NULL,
-    created_at TIMESTAMPTZ DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS logs (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    function_id UUID REFERENCES functions(id) ON DELETE CASCADE,
-    log_message STRING NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT now()
+    requirements TEXT DEFAULT NULL,
+    deployment_yaml TEXT DEFAULT NULL,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
 );
 
 -- Create Indexes for Performance
 CREATE INDEX IF NOT EXISTS idx_functions_state ON functions(state);
-CREATE INDEX IF NOT EXISTS idx_logs_function_id ON logs(function_id);
+
+-- Create a Changefeed for WebSocket notifications (CockroachDB Changefeed)
+CREATE CHANGEFEED FOR functions
+    INTO 'kafka://kafka-service:9092'
+    WITH updated, resolved;
