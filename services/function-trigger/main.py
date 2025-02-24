8788693d8df0b964809c9c7b12fe7c865a898a62
Alireza Dehghan Nayeri <alireza79.nayeri@gmail.com>
2025-02-24 23:49:56 +0200

    * added needed installations for psycopg2-binary to dockerfiles
    * fixed uvicorn
    
    * changed psycopg2 to psycopg2-binary

--- services/function-trigger/main.py
+++ services/function-trigger/main.py
@@ -1,5 +1,4 @@
 import uvicorn
-from app.app import app  # Import FastAPI app from app/app.py
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
